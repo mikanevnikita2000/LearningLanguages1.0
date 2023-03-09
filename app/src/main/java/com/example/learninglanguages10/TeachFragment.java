6bd250fbaf1582fad21be6ca40d89871d24f2725
mikanevnikita2000 <86831041+mikanevnikita2000@users.noreply.github.com>
2023-03-09 14:51:43 +0300

    LearningLanguages1.0

---
+++ app/src/main/java/com/example/learninglanguages10/TeachFragment.java
@@ -0,0 +1,34 @@
+package com.example.learninglanguages10;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TeachFragment extends Fragment {
+
+
+    public TeachFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View viewReturn = inflater.inflate(R.layout.fragment_teach, container, false);
+
+        return viewReturn;
+    }
+}
